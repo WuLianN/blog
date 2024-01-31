101c4a9ce31eaf87023ae6d06df2037ffccc4c92
WuLianN <610575024@qq.com>
2024-01-31 22:00:02 +0800

    feat: 添加通用getUserId函数

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -1,4 +1,19 @@
+import { router } from '@/router'
+import type { LocationQueryValue } from 'vue-router'
+
 export function getToken() {
   const token = localStorage.getItem('token')
   return token
+}
+
+export function getUserId(): (LocationQueryValue | LocationQueryValue[]) {
+  const { path } = router.currentRoute.value
+  const regexp = /\/user\/(\d+)/
+  const match = path.match(regexp)
+
+  if (match) {
+    return match?.[1]
+  }
+
+  return null
 }
