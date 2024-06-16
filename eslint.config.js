6c3e336730ce4e2ff12e3942ae826d8201ad01ee
WuLianN <610575024@qq.com>
2024-06-16 20:44:32 +0800

    chore: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  typescript: true,
-  vue: true,
-})
+export default antfu(
+  {
+    typescript: true,
+    vue: true,
+  },
+  {
+    rules: {
+      'regexp/no-unused-capturing-group': 'off',
+      'regexp/strict': 'off',
+    },
+  },
+)
