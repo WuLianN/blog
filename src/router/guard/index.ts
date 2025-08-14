94093ffc169545bb22f09092f3ea3c3326fe3e90
WuLianN <610575024@qq.com>
2025-08-14 23:04:25 +0800

    feat: 路由跳转 添加标题

--- src/router/guard/index.ts
+++
@@ -1,6 +0,0 @@
-// import { createPermissionGuard } from './permissionGuard'
-// import type { Router } from 'vue-router'
-
-// export function setupRouterGuard(router: Router) {
-//   createPermissionGuard(router)
-// }
--- src/router/index.ts
+++ src/router/index.ts
@@ -20,21 +20,31 @@ const routes = [
   { path: '/login', name: 'login', component: Login },
   { path: '/user/:id', name: 'user', component: Home },
   { path: '/drafts/:id', name: 'drafts', component: Editor },
-  { path: '/draftBox', name: 'draftBox', component: DraftBox },
+  { path: '/draftBox', name: 'draftBox', component: DraftBox, meta: { title: '草稿箱' } },
   { path: '/viewer/:id', name: 'viewer', component: Viewer },
-  { path: '/articleManage', name: 'articleManage', component: ArticleManage },
-  { path: '/setting', name: 'setting', component: Setting },
+  { path: '/articleManage', name: 'articleManage', component: ArticleManage, meta: { title: '文章管理' } },
+  { path: '/setting', name: 'setting', component: Setting, meta: { title: '设置' } },
   { path: '/upload', name: 'upload', component: Upload },
   { path: '/manageImages', name: 'manageImages', component: ManageImages },
   { path: '/design/login', name: 'loginDesign', component: LoginDesign },
-  { path: '/toolkit', name: 'toolkit', component: Toolkit },
-  { path: '/toolkit/json-editor', name: 'JsonEditor', component: JsonEditor },
+  { path: '/toolkit', name: 'toolkit', component: Toolkit, meta: { title: '工具箱' } },
+  { path: '/toolkit/json-editor', name: 'JsonEditor', component: JsonEditor, meta: { title: 'JSON 编辑器' } },
   { path: '/:pathMatch(.*)', component: NotFoundComponent },
 ]
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, _from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  else {
+    document.title = 'blog'
+  }
+  next()
 })
 
 export function setupRouter(app: App<Element>) {
