d634c41c266013c89310bf34e910a9e5a77bf738
WuLianN <610575024@qq.com>
2025-08-14 23:11:09 +0800

    chore: 移除无用代码

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import App from './App.vue'
 import { setupRouter } from './router/index.ts'
-// import { setupRouterGuard } from '@/router/guard'
 import { setupStore } from '@/store'
 import { setupGlobDirectives } from '@/directvies'
 import 'element-plus/theme-chalk/dark/css-vars.css'
@@ -16,9 +15,6 @@ async function setup() {
 
   // 配置路由
   setupRouter(app)
-
-  // 路由守卫
-  // setupRouterGuard(router)
 
   // 全局指令
   setupGlobDirectives(app)
